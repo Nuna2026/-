c962a1e97905299e7545e45e0b8c20f3b76365ff
Nuna2026 <nana.thanchanok@gmail.com>
2018-09-30 15:35:52 +0700

    my neam

---
+++ src/My neam.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './My neam.css';
+class App extends Component {
+ render() {
+ return (
+ <div className="Neam">
+ Hello World
+ </div>
+ );
+ }
+}
+export default My neam;
